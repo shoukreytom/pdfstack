ba1549a752b333079fdeaff2375805b44e2f3bf3
shoukreytom <shoukreytom01@gmail.com>
2020-10-04 07:58:18 +0200

    added equality to debug_value

--- pdfstack/settings.py
+++ pdfstack/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ('P_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ('P_DEBUG_VALUE')
+DEBUG = os.environ('P_DEBUG_VALUE') == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pdfstack.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
