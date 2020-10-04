93411dafe015ae1aaf768f566f9de36dea4780a2
shoukreytom <shoukreytom01@gmail.com>
2020-10-04 08:04:04 +0200

    changed enivron vars

--- pdfstack/settings.py
+++ pdfstack/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('P_SECRET_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ('P_DEBUG_VALUE') == "True"
+DEBUG = os.environ('DEBUG_VALUE') == "True"
 
 ALLOWED_HOSTS = ['pdfstack.herokuapp.com', '127.0.0.1']
 
