9440ecfc5269bfe1c466bd79fbe5446a78c248d2
shoukreytom <shoukreytom01@gmail.com>
2020-10-05 01:50:29 +0200

    fixed bug on model Book

--- home/models.py
+++ home/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Book(models.Model):
-    book = models.FileField(upload_to='books/')
+    book = models.FileField(upload_to='books')
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.book.path.split('/')[-1]}"
+        return f"{self.book.name.split('/')[-1]}"
