6aa9f3b56a6315220fc73049cc7439a0215030e0
shoukreytom <shoukreytom01@gmail.com>
2020-10-05 02:30:45 +0200

    added delete_book signal

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 
 
 class Book(models.Model):
@@ -8,3 +10,8 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.book.name.split('/')[-1]}"
+
+
+@receiver(post_delete, sender=Book)
+def delete_book(sender, instance, **kwargs):
+    instance.book.delete()
