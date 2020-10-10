12b6e42df4a746dc4d95300f4da0f5f9bb71c39e
shoukreytom <shoukreytom01@gmail.com>
2020-10-10 05:26:37 +0200

    added widget with attrs::accept & name to book field

--- home/forms.py
+++ home/forms.py
@@ -4,7 +4,10 @@ from .models import Book
 
 
 class UploadBook(forms.ModelForm):
-    book = forms.FileField(label='')
+    book = forms.FileField(label='', widget=forms.FileInput(attrs={
+        'accept': 'application/pdf',
+        'name': 'pdf_file',
+    }))
 
     class Meta:
         model = Book
