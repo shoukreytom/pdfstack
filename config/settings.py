dfef245725ad04106c1dd506716223a762271bcd
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-02-13 04:21:12 +0200

    restructure project

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pdfstack.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -60,7 +60,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pdfstack.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
