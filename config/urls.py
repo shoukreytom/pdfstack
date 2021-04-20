fd3fefda0437dba6e6ea7ec0fb7bea7700a6a9a5
Abdul Shakoor Mousa <abdulshakoor.abdulazeem@gmail.com>
2021-04-20 06:46:59 +0200

    fix upload pdf problem (not confirmed)

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('account/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/static/js/viewer.js
+++ home/static/js/viewer.js
@@ -1,13 +1,11 @@
 const book_url = $("#bklink").val();
 console.log(book_url);
 var loadingPage = pdfjsLib.getDocument(book_url);
-console.log(`http://127.0.0.1:8000${book_url}`);
 loadingPage.promise.then((doc) => {
     const numPages = doc._pdfInfo.numPages;
     doc.getPage(1).then((page) => {
         var canvas = document.getElementById("viewer");
         var context = canvas.getContext("2d");
-        console.log(page);
         var viewport = page.getViewport(1);
         canvas.width = viewport.width;
         canvas.height = viewport.height;
