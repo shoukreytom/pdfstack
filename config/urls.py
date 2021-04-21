9d8764e8ead7cb989ded9dcde050e9c2f0996886
Abdul Shakoor Mousa <abdulshakoor.abdulazeem@gmail.com>
2021-04-21 19:34:48 +0200

    serve static in both cases of DEBUG value (True or Flase)

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('account/', include('users.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
